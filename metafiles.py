c303d332e68bf3ecb8442405cf1d02e33296ff01
Koen Kusters <c.h.j.kusters@tue.nl>
2023-05-19 10:53:11 +0200

    Added .yaml and .sh for metafiles generation

--- metafiles.py
+++ metafiles.py
@@ -1,5 +1,8 @@
+import os
 from dinov2.data.datasets import ImageNet
 
+dataroot = '/share/medical/Vault/datasets_working/GastroNet5MDino'
+
 for split in ImageNet.Split:
-    dataset = ImageNet(split=split, root="<ROOT>", extra="<Extra>")
+    dataset = ImageNet(split=split, root=dataroot, extra='')
     dataset.dump_extra()
