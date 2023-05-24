0f92e5d0aa695b7035d3ba4af07e18a4467a51af
Koen Kusters <c.h.j.kusters@tue.nl>
2023-05-24 13:31:12 +0200

    Added .yaml and .sh for metafiles generation

--- metafiles.py
+++ metafiles.py
@@ -1,8 +1,17 @@
 import os
 from dinov2.data.datasets import ImageNet
 
+# Specify dataroot
 dataroot = '/share/medical/Vault/datasets_working/GastroNet5MDino'
 
+# Create labels.txt file in dataroot
+with open(os.path.join(dataroot, 'labels.txt'), 'w') as f:
+    for root, dirs, files in os.walk(dataroot):
+        for dir in dirs:
+            if dir != 'train' and dir != 'val' and dir != 'test':
+                f.write('{},{}\n'.format(dir, dir))
+
 for split in ImageNet.Split:
-    dataset = ImageNet(split=split, root=dataroot, extra='')
-    dataset.dump_extra()
+    if 'TRAIN' in '{}'.format(split):
+        dataset = ImageNet(split=split, root=dataroot, extra=dataroot)
+        dataset.dump_extra()
