1f9556fef21ced2184c0f1e8ba985b6cf6578c6c
Koen Kusters <c.h.j.kusters@tue.nl>
2023-06-16 10:20:36 +0200

    Modifications

--- run_train.sh
+++ run_train.sh
@@ -11,6 +11,6 @@ python3 setup.py install --user
 
 ### ImageNet Implementation ###
 python3 dinov2/train/train.py \
-    --config-file dinov2/configs/train/vitl16.yaml \
+    --config-file dinov2/configs/train/vitl16_short.yaml \
     --output-dir '/share/medical/chjkusters/dinov2/output/' \
     train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/Imagenet':extra='/share/medical/Vault/datasets_working/Imagenet'
