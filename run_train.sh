6d451d5d393f8ed1738756260ce7ff8f32b6f2a8
Koen Kusters <c.h.j.kusters@tue.nl>
2023-06-16 10:48:28 +0200

    Modifications

--- run_train.sh
+++ run_train.sh
@@ -3,15 +3,15 @@ cd /share/medical/chjkusters/dinov2/
 export HOME=/share/medical/chjkusters/dinov2install/
 python3 setup.py install --user
 
-#### GastroNet Implementation ###
-#python3 dinov2/train/train.py \
-#    --config-file dinov2/configs/train/vits16.yaml \
-#    --output-dir '/share/medical/chjkusters/dinov2/output/' \
-#    train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/GastroNet5MDino':extra='/share/medical/Vault/datasets_working/GastroNet5MDino'
-
-### ImageNet Implementation ###
+### GastroNet Implementation ###
 python3 dinov2/train/train.py \
-    --nodes 1 \
-    --config-file dinov2/configs/train/vitl16_short.yaml \
+    --config-file dinov2/configs/train/vitl_short.yaml \
     --output-dir '/share/medical/chjkusters/dinov2/output/' \
-    train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/Imagenet':extra='/share/medical/Vault/datasets_working/Imagenet'
+    train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/GastroNet5MDino':extra='/share/medical/Vault/datasets_working/GastroNet5MDino'
+
+#### ImageNet Implementation ###
+#python3 dinov2/train/train.py \
+#    --nodes 1 \
+#    --config-file dinov2/configs/train/vitl16_short.yaml \
+#    --output-dir '/share/medical/chjkusters/dinov2/output/' \
+#    train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/Imagenet':extra='/share/medical/Vault/datasets_working/Imagenet'
