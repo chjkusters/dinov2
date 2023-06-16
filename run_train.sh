d51bdab42e5ac14786709bc99c386df5239462a9
Koen Kusters <c.h.j.kusters@tue.nl>
2023-06-16 11:09:47 +0200

    Modifications

--- run_train.sh
+++ run_train.sh
@@ -3,11 +3,19 @@ cd /share/medical/chjkusters/dinov2/
 export HOME=/share/medical/chjkusters/dinov2install/
 python3 setup.py install --user
 
+#### GastroNet Implementation ###
+#python3 dinov2/train/train.py \
+#    --config-file dinov2/configs/train/vitl16_short.yaml \
+#    --output-dir '/share/medical/chjkusters/dinov2/output/' \
+#    train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/GastroNet5MDino':extra='/share/medical/Vault/datasets_working/GastroNet5MDino'
+
 ### GastroNet Implementation ###
-python3 dinov2/train/train.py \
+python3 dinov2/run/train/train.py \
+    --nodes 1 \
     --config-file dinov2/configs/train/vitl16_short.yaml \
     --output-dir '/share/medical/chjkusters/dinov2/output/' \
     train.dataset_path=ImageNet:split=TRAIN:root='/share/medical/Vault/datasets_working/GastroNet5MDino':extra='/share/medical/Vault/datasets_working/GastroNet5MDino'
+
 
 #### ImageNet Implementation ###
 #python3 dinov2/train/train.py \
